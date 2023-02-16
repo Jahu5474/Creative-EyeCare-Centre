ff29d017e38498a9ac6eadd9008ecb9c6e1e4416
jahu5474 <jasper123ya@live.com>
2023-02-15 23:32:44 -0800

    added responsiveness

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar'
 import { Divider } from '@chakra-ui/react'
 
+
 interface LayoutProps {
   children: React.ReactNode;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,48 @@
 import { NextPage } from 'next';
-import {Box, Flex, Container, Image, Heading} from "@chakra-ui/react"
+import {Box, Flex, Container, Image, Heading, Text, Stack} from "@chakra-ui/react"
 import styles from '@emotion/styled'
 
 const Home: NextPage = ()=>{
   return (
-
       <Container
         display="flex"
-        mt="20px"
+        mt="30px"
         maxW="container.lg"
       >
+      <Stack
+      display={{base:"flex",md:"flex"}}
+      direction={{base:"column", md:"row"}}
+      width={{base:"auto", md:"none"}}
+      flexGrow={1}
+      align="center"
+    >
         <Box
           width="50%"
           display="flex"
           alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+          mr={2}
         >
           <Heading>Creative EyeCare Centre Inc.</Heading>
+          <Text p="10px">
+            Proudly serving you in Downtown Vancouver, BC since 1996.
+            Get the best selection of eyewear and contact lenses.
+          </Text>
         </Box>
         <Box
           width="50%"
         >
           <Image 
-            boxSize="sm" 
-            borderRadius="full"
+            display={{base:"none", md:"flex"}}
+            boxShadow="lg"
+            objectFit="cover"
+            borderRadius="md"
             src="/images/jimin.png" alt="jimin"/>
         </Box>
+      </Stack>
       </Container>
+
 
   )
 };
