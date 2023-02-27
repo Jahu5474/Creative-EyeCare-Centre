77b961cd326c5591adfcb8ef294454b03d415df5
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-02-26 19:46:55 -0800

    added contact lens content

--- src/pages/contactlens.tsx
+++ src/pages/contactlens.tsx
@@ -1,10 +1,107 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react'
+import { Box, Container, Stack, Image, Text, Heading, Card, CardBody } from '@chakra-ui/react'
+import Link from 'next/link';
+import {motion} from 'framer-motion'
 
 const contactlens = () =>{
     return(
         <>
-            <Box>Hello</Box>
+            <Container
+                display="flex"
+                mt="30px"
+                m="auto"
+                maxW="container.lg"
+                minH="60vh"
+                centerContent
+            >
+            <motion.div
+                initial={{opacity: 0}}
+                animate={{opacity: 1, x: 2}}
+                transition={{duration:0.8}}
+            >
+                <Stack
+                    display={{ base: "flex", md: "flex" }}
+                    direction={{ base: "column", md: "row" }}
+                    width={{ base: "auto", md: "none" }}
+                    spacing={5}
+                    mt="4"
+                    mb="4"
+                >
+                    <Card maxW='xs' display="flex">
+                        <CardBody>
+                            <Link href="/">
+                            <Image
+                                src='/images/PRECISION-1-DAY-90PACK.png'
+                                alt='Precision'
+                                borderRadius='md'
+                            />
+                            <Stack mt='6' spacing='3'>
+                            <Heading size='sm'>Precision 1 Day 90PK</Heading>
+                            <Text color='orange.400' fontSize='xl'>
+                                $72
+                            </Text>
+                            </Stack>
+                            </Link>
+                        </CardBody>
+                    </Card>
+
+                    <Card maxW='xs' display="flex">
+                        <CardBody>
+                            <Link href="/">
+                            <Image
+                                src='/images/PRECISION-1-DAY-90PACK.png'
+                                alt='Precision'
+                                borderRadius='md'
+                            />
+                            <Stack mt='6' spacing='3'>
+                            <Heading size='sm'>Precision 1 Day 90PK</Heading>
+                            <Text color='orange.400' fontSize='xl'>
+                                $72
+                            </Text>
+                            </Stack>
+                            </Link>
+                        </CardBody>
+                    </Card>
+
+                    <Card maxW='xs' display="flex">
+                     <CardBody>
+                        <Link href="/">
+                        <Image
+                            src='/images/PRECISION-1-DAY-90PACK.png'
+                            alt='Precision'
+                            borderRadius='md'
+                        />
+                        <Stack mt='6' spacing='3'>
+                        <Heading size='sm'>Precision 1 Day 90PK</Heading>
+                        <Text color='orange.400' fontSize='xl'>
+                            $72
+                        </Text>
+                        </Stack>
+                        </Link>
+                     </CardBody>
+                    </Card>
+
+                    <Card maxW='xs' display="flex">
+                     <CardBody>
+                        <Link href="/">
+                        <Image
+                            src='/images/PRECISION-1-DAY-90PACK.png'
+                            alt='Precision'
+                            borderRadius='md'
+                        />
+                        <Stack mt='6' spacing='3'>
+                        <Heading size='sm'>Precision 1 Day 90PK</Heading>
+                        <Text color='orange.400' fontSize='xl'>
+                            $72
+                        </Text>
+                        </Stack>
+                        </Link>
+                     </CardBody>
+                    </Card>
+
+                </Stack>
+                </motion.div>
+            </Container>
         </>
     )
 }
