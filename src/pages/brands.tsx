844e84e19d6b7a64918884bf321482959960761a
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-03-14 21:10:39 -0700

    Image added for brands.

--- src/pages/brands.tsx
+++ src/pages/brands.tsx
@@ -12,8 +12,9 @@ const Brands = () => {
                         size="lg"
                         mt={4}
                         mb={4}
+                        textAlign="center"
                     >
-                        Brands We Carry:
+                        Brands We Carry
                     </Heading>
                     <Grid
                         templateColumns={{
@@ -32,6 +33,33 @@ const Brands = () => {
                         </GridItem>
                         <GridItem>
                             <Image src="/images/Prada-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Celine-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Saint-Laurent-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Lindberg.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Chloe-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Bottega-Veneta-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Chanel-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Givenchy-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/Dolce-And-Gabbana-Logo.png" />
+                        </GridItem>
+                        <GridItem>
+                            <Image src="/images/RayBan-Logo.png" />
                         </GridItem>
                     </Grid>
                 </Stack>
