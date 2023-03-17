78b7730a3175a89d1775478b43f6c0e0c4c3f4ad
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-03-17 16:10:27 -0700

    tried to fix a bug but didnt work

--- src/components/contactForm/contactForm.tsx
+++ src/components/contactForm/contactForm.tsx
@@ -16,49 +16,13 @@ import { useRecoilState } from "recoil";
 import Link from "next/link";
 
 const ContactForm = () => {
-    //     const [formData, setFormData] = useRecoilState(formState);
+    function callButton() {
+        const Number = "604-423-3603"
 
-    //     const handleInputChange = (e) => {
-    //         setFormData({ ...formData, [e.target.name]: e.target.value });
-    //     };
-
-    //     const onSubmit = async (e) => {
-    //         e.preventDefault();
-
-    //         const data = {
-    //             to: "juhynjuhn@gmail.com",
-    //             from: formData.email,
-    //             subject: "New message from contact form",
-    //             html: `<p>Name: ${formData.name}</p>
-    //                  <p>Email: ${formData.email}</p>
-    //                  <p>Message: ${formData.message}</p>`,
-    //         };
-
-    //         try {
-    //             const res = await fetch("/api/send-email", {
-    //                 method: "POST",
-    //                 headers: {
-    //                     "Content-Type": "application/json",
-    //                 },
-    //                 body: JSON.stringify(data),
-    //             });
-
-    //             if (res.status === 200) {
-    //                 alert("Your message was sent successfully!");
-    //                 setFormData({
-    //                     name: "",
-    //                     email: "",
-    //                     message: "",
-    //                 });
-    //             } else {
-    //                 alert("There was an error sending your message. Please try again.");
-    //             }
-    //         } catch (err) {
-    //             console.error(err);
-    //             alert("There was an error sending your message. Please try again.");
-    //         }
-    //     };
-
+        function handleClick() {
+            window.location.href = `tel:${Number}`
+        }
+    }
 
 
     return (
@@ -97,51 +61,24 @@ const ContactForm = () => {
                             Send us a Email!
                         </Button>
                     </Link>
+                    <Button >
+                        (604)423-3603
+                    </Button>
+
                 </Stack>
 
 
 
-                {/* <form onSubmit={onSubmit}>
-                    <FormControl id="name" mb={4} mt={4}>
-                        <FormLabel fontWeight="bold">Name:</FormLabel>
-                        <Input
-                            type="text"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </FormControl>
-                    <FormControl id="email" mb={4}>
-                        <FormLabel fontWeight="bold">Email:</FormLabel>
-                        <Input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </FormControl>
-                    <FormControl id="message" mb={4}>
-                        <FormLabel fontWeight="bold">Message:</FormLabel>
-                        <Textarea
-                            name="message"
-                            value={formData.message}
-                            onChange={handleInputChange}
-                            required
-                        />
-                    </FormControl>
-                    <Link href="mailto:juhynjuhn@gmail.com">
-                        <Button>Submit</Button>
-                    </Link>
-                </form> */}
+
 
             </Container>
 
         </>
 
     )
-};
+}
+
+
 
 
 export default ContactForm;
