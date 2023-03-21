2a67d9262e429333bde19b2576cca87f35bbb2fd
Jasper Huynh <107389291+Jahu5474@users.noreply.github.com>
2023-03-20 22:01:40 -0700

    added TOS content

--- src/pages/TOS.tsx
+++ src/pages/TOS.tsx
@@ -12,11 +12,11 @@ const TOS = () => {
                     direction="column"
                     justify="center"
                     align="center"
+                    gap="3"
                 >
                     <Heading
                         fontSize="2xl"
-                        mt={4}
-                        mb={4}
+
                     >
                         Terms of Conditions
                     </Heading>
@@ -28,8 +28,19 @@ const TOS = () => {
                     </Heading>
                     <Text
                         w="70%"
+
                     >
                         If you are our regular/repeat contact lens customers, you don&apos;t need to provide your prescription paper to place an order on our website since we already have your prescription on paper file. For new customers, yes, we highly recommend ordering from a valid, updated prescription. Getting a comprehensive eye exam at least every two years ensures your eyes are healthy and the eyewear you purchase is suited to your vision needs.
+                    </Text>
+                    <Heading
+                        as="h2"
+                        fontSize="2xl"
+                    >
+                        Can I use my eyeglasses prescription to order contact lenses?
+                    </Heading>
+                    <Text>
+                        Eyeglasses and contact lenses require two different prescriptions, for a couple of reasons:
+
                     </Text>
                 </Stack>
             </Container>
